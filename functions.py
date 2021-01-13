661074503732cd86af820bc7a2f9caa9a0e81f3c
Tomasz Pichlinski <tomaszpichlinski2@gmail.com>
2021-01-13 21:09:16 +0100

    Susoku solver

--- functions.py
+++ functions.py
@@ -1,13 +1,10 @@
-from bs4 import BeautifulSoup
 import requests
-import random
+from bs4 import BeautifulSoup
+
 
 def populate(numbers):
-    link_strona = []
-    with open('link.txt', 'r') as f:
-        for line in f:
-            link_strona.append(line[:-1])
-    html_content = requests.get(link_strona[0]).text
+    link_strona = 'http://mojesudoku.pl/gra-sudoku/trywialna_abiabfj.html'
+    html_content = requests.get(link_strona).text
     soup = BeautifulSoup(html_content, "lxml")
     Grid = []
     Square_row = []
@@ -21,28 +18,28 @@ def populate(numbers):
 
     for link in soup.find_all("td", {"class": "sudokuInnerCell"}):
         number_in_square = str(link)[37]
-        if (number_in_square not in numbers):
+        if number_in_square not in numbers:
             number_in_square = ' '
-        if(number_in_square in numbers):
+        if number_in_square in numbers:
             wpisane.append(licznik_wpisanych)
-        if(licznik_row<3):
+        if licznik_row < 3:
             Row.append(number_in_square)
-            licznik_row+=1
-            if(licznik_row==3):
-                licznik_row=0
+            licznik_row += 1
+            if licznik_row == 3:
+                licznik_row = 0
                 Square.append(Row)
-                Row=[]
-                licznik_square+=1
-                if(licznik_square==3):
-                    licznik_square=0
+                Row = []
+                licznik_square += 1
+                if licznik_square == 3:
+                    licznik_square = 0
                     Square_row.append(Square)
-                    Square=[]
-                    licznik_square_row+=1
-                    if(licznik_square_row==3):
-                        licznik_square_row=0
+                    Square = []
+                    licznik_square_row += 1
+                    if licznik_square_row == 3:
+                        licznik_square_row = 0
                         Grid.append(Square_row)
-                        Square_row=[]
-        licznik_wpisanych+=1
+                        Square_row = []
+        licznik_wpisanych += 1
     return [Grid, wpisane]
 
 
@@ -56,13 +53,14 @@ def display(Grid):
             print("")
         print(" ")
 
+
 def square_check(number_to_check, row_of_squares, square, grid):
     for x in range(3):
         for y in range(3):
             if number_to_check == grid[row_of_squares][square][x][y]:
-
                 return False
     return True
+
 
 def row_check(number_to_check, row_of_squares, row, grid):
     for x in range(3):
@@ -70,6 +68,7 @@ def row_check(number_to_check, row_of_squares, row, grid):
             if number_to_check == grid[row_of_squares][x][row][y]:
                 return False
     return True
+
 
 def column_check(number_to_check, square, number_in_square, grid):
     for x in range(3):
@@ -89,22 +88,22 @@ def check_if_correct(row_of_squares, square, row, number_in_square, grid, number
                 return True
     return False
 
+
 def find_empty(new_Grid):
     for x in range(3):
-         for y in range(3):
+        for y in range(3):
             for z in range(3):
-                 for a in range(3):
+                for a in range(3):
                     if new_Grid[x][y][z][a] == ' ':
-                        return [x,y,z,a]
+                        return [x, y, z, a]
     return None
-
 
 
 def solve(new_Grid):
     find = find_empty(new_Grid)
     if not find:
         return True
-    for i in range(1,10):
+    for i in range(1, 10):
         if check_if_correct(find[0], find[1], find[2], find[3], new_Grid, str(i)):
             new_Grid[find[0]][find[1]][find[2]][find[3]] = str(i)
             if solve(new_Grid):
--- main.py
+++ main.py
@@ -1,60 +1,31 @@
-import win32con
-
-import functions
-from win32 import win32gui
-numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
 import copy
 import time
+
 import pyautogui
 
-while True:
-    populate_array = functions.populate(numbers)
-    Original_grid = populate_array[0]
-    new_Grid = copy.deepcopy(Original_grid)
-    functions.solve(new_Grid)
-    check = -1
-    print("XD")
-    pyautogui.keyDown('alt')
-    pyautogui.keyDown('tab')
-    pyautogui.keyUp('alt')
-    pyautogui.keyUp('tab')
-    pyautogui.click(x=400, y=400)
-    pyautogui.press('tab')
-    time.sleep(1)
-    for x in range(3):
-        for y in range(3):
-            for z in range(3):
-                for a in range(3):
-                    check += 1
-                    if check in populate_array[1]:
-                        continue
-                    else:
-                        pyautogui.press(new_Grid[x][y][z][a])
-                        pyautogui.press('tab')
-    pyautogui.press('enter')
-    time.sleep(2)
-    pyautogui.click(x=1265, y=321)
-    time.sleep(3)
-    pyautogui.click(x=650, y=800)
-    time.sleep(3)
-    pyautogui.click(x=539, y=52)
-    pyautogui.keyDown('ctrl')
-    pyautogui.keyDown('c')
-    pyautogui.keyUp('ctrl')
-    pyautogui.keyUp('c')
-    pyautogui.keyDown('alt')
-    pyautogui.keyDown('tab')
-    pyautogui.keyUp('alt')
-    pyautogui.keyUp('tab')
-    pyautogui.keyDown('ctrl')
-    pyautogui.keyDown('a')
-    pyautogui.keyUp('ctrl')
-    pyautogui.keyUp('a')
-    pyautogui.keyDown('ctrl')
-    pyautogui.keyDown('v')
-    pyautogui.keyUp('ctrl')
-    pyautogui.keyUp('v')
-    pyautogui.keyDown('ctrl')
-    pyautogui.keyDown('s')
-    pyautogui.keyUp('ctrl')
-    pyautogui.keyUp('s')
+import functions
+
+numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
+
+populate_array = functions.populate(numbers)
+Original_grid = populate_array[0]
+new_Grid = copy.deepcopy(Original_grid)
+functions.solve(new_Grid)
+check = -1
+pyautogui.keyDown('alt')
+pyautogui.keyDown('tab')
+pyautogui.keyUp('alt')
+pyautogui.keyUp('tab')
+pyautogui.click(x=400, y=400)
+pyautogui.press('tab')
+time.sleep(1)
+for x in range(3):
+    for y in range(3):
+        for z in range(3):
+            for a in range(3):
+                check += 1
+                if check in populate_array[1]:
+                    continue
+                else:
+                    pyautogui.press(new_Grid[x][y][z][a])
+                    pyautogui.press('tab')
